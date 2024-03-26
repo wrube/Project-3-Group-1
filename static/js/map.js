07659522b4969aedb25d3e43bc6924f245755bf3
wrube <bjminwruck@gmail.com>
2024-03-27 06:37:46 +1030

    changed code to allow country to be a COA or COA

--- static/js/map.js
+++ static/js/map.js
@@ -1,43 +1,52 @@
 // d3.json('data/asylum_decisions_2008-2023.json')
 
-const COA = "AUS";
-const decisionsCOAfiltered = filterByAttribute(decisions, "coa_iso", COA);
+const countryOfInterest = "UKR";
 
+generateLayersAndMap(countryOfInterest, true);
 
-const decisionsObject = {
-    Recognised: {attributeKey: "dec_recognized",
-                title: "Total Asylum Seekers Recognised 2008-2023"
-                },
-    Other: {attributeKey: "dec_other",
-                title: "Total 'Other' Decisions  2008-2023"
-    },
-    Rejected: {attributeKey: "dec_rejected",
-                title: "Total Asylum Seekers Rejected 2008-2023"
-    },
-    Closed: {attributeKey: "dec_closed",
-                title: "Total Closed Decisions 2008-2023"
-    },
-    Total: {attributeKey: "dec_total",
-                title: "Total Applications 2008-2023"
-},
-}
+function generateLayersAndMap(countryOfInterest, countryIsCOA){
+    const decisionsFiltered = filterByAttribute(decisions, countryIsCOA, countryOfInterest);
 
-// const decisionLayers = ;
+    
+    console.log(decisionsFiltered)
 
-for (const key in decisionsObject) {
-    if (Object.hasOwnProperty.call(decisionsObject, key)) {
-        const dec = decisionsObject[key];
-        dec["layer"] = country_layer(countries, decisionsCOAfiltered, dec.attributeKey);
-        dec["legend"] = createLegend(dec.layer, dec.title);
-
+    // create an object that contains all the decision options and titles for map generation
+    const decisionsObject = {
+        Recognised: {attributeKey: "dec_recognized",
+                    title: "Total Asylum Seekers Recognised 2008-2023"
+                    },
+        Other: {attributeKey: "dec_other",
+                    title: "Total 'Other' Decisions  2008-2023"
+        },
+        Rejected: {attributeKey: "dec_rejected",
+                    title: "Total Asylum Seekers Rejected 2008-2023"
+        },
+        Closed: {attributeKey: "dec_closed",
+                    title: "Total Closed Decisions 2008-2023"
+        },
+        Total: {attributeKey: "dec_total",
+                    title: "Total Applications 2008-2023"
+        },
     }
+
+    for (const key in decisionsObject) {
+        if (Object.hasOwnProperty.call(decisionsObject, key)) {
+            const dec = decisionsObject[key];
+            dec["layer"] = country_layer(countries, decisionsFiltered, dec.attributeKey, countryIsCOA);
+            dec["legend"] = createLegend(dec.layer, dec.title);
+
+        }
+    }
+    createMap(decisionsObject);
 }
 
-createMap(decisionsObject);
-
-
-function filterByAttribute(data, attribute, value) {
-    return data.filter(item => item[attribute] == value);
+function filterByAttribute(data, countryIsCOA, value) {
+    if (countryIsCOA == true) {
+        return data.filter(item => item["coa_iso"] == value)
+    } else {
+        return data.filter(item => item["coo_iso"] == value)
+    }
+    ;
   }
 
 function sumDecision(decisionArray, decisionType) {
@@ -51,21 +60,24 @@ function sumDecision(decisionArray, decisionType) {
     return total
 }
 
-function addDecisionTotalToCountry(geoJSON, decisionList, decisionType) {
+function addDecisionTotalToCountry(geoJSON, decisionList, decisionType, countryIsCOA) {
     const data = geoJSON.features;
-    const modifiedCountries = [];
-    const logString = `log10_${decisionType}`;
-    
+    const modifiedCountries = [];    
 
     data.forEach(country => {
 
         const countryISO3 = country.properties.ISO_A3;
         // create a filtered of countries list by the current country feature
-        const countryDecision = filterByAttribute(decisionList, "coo_iso", countryISO3);
+        let countryDecision;
+        if (countryIsCOA === true) {
+            countryDecision = filterByAttribute(decisionList, false, countryISO3);
+        } else {
+            countryDecision = filterByAttribute(decisionList, true, countryISO3);
+        }
+        
         const totalDecision = sumDecision(countryDecision, decisionType);
 
         country.properties[decisionType] = totalDecision;
-        country.properties[logString] = log10(totalDecision);
 
         modifiedCountries.push(country);
 
@@ -76,8 +88,8 @@ function addDecisionTotalToCountry(geoJSON, decisionList, decisionType) {
 }
 
 
-function country_layer(geoJSON, decisionList, decisionType) {    
-    const c = addDecisionTotalToCountry(geoJSON, decisionList, decisionType);
+function country_layer(geoJSON, decisionList, decisionType, countryIsCOA) {    
+    const c = addDecisionTotalToCountry(geoJSON, decisionList, decisionType, countryIsCOA);
 
     console.log(decisionType);
 
@@ -85,7 +97,6 @@ function country_layer(geoJSON, decisionList, decisionType) {
     const cLayer = L.choropleth(c, {
 
         // Define which property in the features to use.
-        // valueProperty: `log10_${decisionType}`,
         valueProperty: decisionType,
     
         // Set the colour scale.
@@ -105,7 +116,6 @@ function country_layer(geoJSON, decisionList, decisionType) {
     
         // Binding a popup to each layer
         onEachFeature: function(feature, layer) {
-        let logString = `log10_${decisionType}`
           layer.bindPopup(`<b>${feature.properties['ADMIN']}</b>
           <br>
           Total ${decisionType}: ${feature.properties[decisionType]}
@@ -142,15 +152,15 @@ function createMap(inputs) {
     // Create our map, giving it the streetmap and earthquakes layers to display on load.
     let map = L.map("map", {
       center: [
-        -25, 125
+        0, 0
       ],
       zoom: 2,
       layers: [base]
     });
 
-    // const legend_1 = createLegend(inputs);
-    // legend_1.addTo(myMap);
 
+
+    // add the relevent legend according to the selected overlay layer
     map.on('overlayadd', function (eventLayer) {
         if (eventLayer.name === 'Recognised') {         
             inputs.Recognised.legend.addTo(map);
@@ -162,10 +172,12 @@ function createMap(inputs) {
             inputs.Closed.legend.addTo(map);
         } else if (eventLayer.name === 'Total') {
             inputs.Total.legend.addTo(map);
+            }
         }
-    }
     );
 
+
+    // remove the relevent legend according to the deselected overlay layer
     map.on('overlayremove', function (eventLayer) {
         if (eventLayer.name === 'Recognised') {
             map.removeControl(inputs.Recognised.legend);           
@@ -178,7 +190,8 @@ function createMap(inputs) {
         } else if (eventLayer.name === 'Total') {
             map.removeControl(inputs.Total.legend);
         }
-    })
+        }
+    )
 
     // Create a layer control.
     // Pass it our baseMaps and overlayMaps.
@@ -188,15 +201,7 @@ function createMap(inputs) {
     }).addTo(map);
 }
 
-function log10(number) {
-    if (number == 0) {
-        return 0
-    } else {
-        resultBase10Log = Math.log(number) / Math.log(10)
-        return resultBase10Log;
-    }
 
-}
 function createLegend (choroplethLayer, title) {
      // Add legend (don't forget to add the CSS from index.html)
     var legend = L.control({ position: 'bottomright' })
