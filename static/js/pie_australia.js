57fc01aea619c4cb7c41e353b2e4b0b564cf2b8f
JollyCJoseph <jisha_jolly@yahoo.com>
2024-03-31 13:02:23 +0530

    changed index file

--- static/js/pie_australia.js
+++ static/js/pie_australia.js
@@ -1,8 +1,9 @@
-let coo="Afghanistan"
-let dec_closed=0
-let dec_other=0
-let dec_recognized=0
-let dec_rejected=0
+//console.log(decisions)
+//let coo="Afghanistan"
+//let dec_closed=0
+//let dec_other=0
+//let dec_recognized=0
+//let dec_rejected=0
 let values = [155,0,13762,3368]
 let labels=['dec_closed','dec_other','dec_recognized','dec_rejected']
 function init() {
@@ -13,18 +14,31 @@ function init() {
     }];
   
     let layout = {
-      height: 600,
-      width: 800
+      height: 400,
+      width: 550
     };
   
-    Plotly.newPlot("pie", data, layout);
-}
+    Plotly.newPlot("graph2", data, layout);
+  };
+for(let j=0; j<1000;j++){
+  var el = document.createElement("option");
+  el.text = decisions[j].coo_name;
+  el.value = decisions[j].coo_name;
+  var select = document.getElementById("selDataset");
+  select.appendChild(el);
+};
+  
 d3.selectAll("#selDataset").on("change", getData); 
 function getData(){
 let data=[]
+let coo="Afghanistan"
+let dec_closed=0
+let dec_other=0
+let dec_recognized=0
+let dec_rejected=0
 let dropdownMenu = d3.select("#selDataset");
-let coo = dropdownMenu.property("value");
-for (let i=0;i<decisions.length;i++){
+ coo = dropdownMenu.property("value");
+for (let i=0;i<1000;i++){
     if(decisions[i].coo_name==coo && decisions[i].coa_name=="Australia"){
         dec_closed+=decisions[i].dec_closed
         dec_other+=decisions[i].dec_other
@@ -32,10 +46,10 @@ for (let i=0;i<decisions.length;i++){
         dec_rejected+=decisions[i].dec_rejected
     }
 }
-values=[dec_closed,dec_other,dec_recognized,dec_rejected]
+data=[dec_closed,dec_other,dec_recognized,dec_rejected]
 updatePlotly(data);
 }
 function updatePlotly(newdata) {
-    Plotly.restyle("pie", "values", [newdata]);
+    Plotly.restyle("graph2", "values", [newdata]);
 }
 init();
