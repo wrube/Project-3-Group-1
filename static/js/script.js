056ac6402c1681b9768d00183df7e8aad22280d3
B-osborne1 <bosb019@gmail.com>
2024-04-01 14:59:54 +1100

    Fixed map sizing issue

--- static/js/script.js
+++ static/js/script.js
@@ -12,5 +12,10 @@ tabs.forEach(tab => {
         })
         tab.classList.add('active')
         target.classList.add('active')
+
+        // If the clicked tab is the one containing the map, invalidate the map size
+        if (tab.dataset.tabTarget === '#pricing') {
+            map1.invalidateSize();
+        }
     })
 })
