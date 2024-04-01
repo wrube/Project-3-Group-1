f6a9dbf0dab7f5df7b0aaa119fcd7df8e3935587
B-osborne1 <bosb019@gmail.com>
2024-04-01 22:27:10 +1100

    Functioning comment section added

--- static/js/script.js
+++ static/js/script.js
@@ -14,3 +14,100 @@ tabs.forEach(tab => {
         target.classList.add('active')
     })
 })
+
+"use strict";
+
+const userId = {
+    name: null,
+    identity: null,
+    image: null,
+    message: null,
+    date: null
+}
+
+const userComment = document.querySelector(".usercomment");
+const publishBtn = document.querySelector("#publish");
+const comments = document.querySelector(".comments");
+const userName = document.querySelector(".user");
+
+// Function to add a new post to local storage and append it to the page
+function addPostToLocalStorage(post) {
+    let storedPosts = localStorage.getItem("comments");
+    if (!storedPosts) {
+        storedPosts = [];
+    } else {
+        storedPosts = JSON.parse(storedPosts);
+    }
+    storedPosts.push(post);
+    localStorage.setItem("comments", JSON.stringify(storedPosts));
+}
+
+// Function to retrieve comments from local storage and append them to the page
+function displayCommentsFromLocalStorage() {
+    const storedPosts = localStorage.getItem("comments");
+    if (storedPosts) {
+        const parsedPosts = JSON.parse(storedPosts);
+        parsedPosts.forEach(post => {
+            appendPostToPage(post);
+        });
+    }
+}
+
+// Function to append a single post to the page
+function appendPostToPage(post) {
+    let published =
+        `<div class="parents">
+            <img src="${post.image}">
+            <div>
+                <h1>${post.name}</h1>
+                <p>${post.message}</p>
+                <div class="engagements">
+                    <img src="like.png">
+                    <img src="share.png">
+                </div>
+                <span class="date">${post.date}</span>
+            </div>
+        </div>`;
+    comments.innerHTML += published;
+}
+
+userComment.addEventListener("input", e => {
+    if (!userComment.value) {
+        publishBtn.setAttribute("disabled", "disabled");
+        publishBtn.classList.remove("abled");
+    } else {
+        publishBtn.removeAttribute("disabled");
+        publishBtn.classList.add("abled");
+    }
+})
+
+function addPost() {
+    if (!userComment.value) return;
+    userId.name = userName.value;
+    if (userId.name === "Anonymous") {
+        userId.identity = false;
+        userId.image = "anonymous.png";
+    } else {
+        userId.identity = true;
+        userId.image = "user.png";
+    }
+
+    userId.message = userComment.value;
+    userId.date = new Date().toLocaleString();
+
+    // Add the new post to local storage
+    addPostToLocalStorage(userId);
+
+    // Append the new post to the page
+    appendPostToPage(userId);
+
+    userComment.value = "";
+
+    let commentsNum = document.querySelectorAll(".parents").length;
+    document.getElementById("comment").textContent = commentsNum;
+}
+
+// Display comments from local storage when the page loads
+displayCommentsFromLocalStorage();
+
+publishBtn.addEventListener("click", addPost);
